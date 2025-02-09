b2e1ab27bca805d3c3cc73dbf828582f0a4d18e6
onionharvester <147471643+onionharvester@users.noreply.github.com>
2025-02-09 00:30:28 -0800

    Create song_reader_tb.v

---
+++ lab4_files/song_reader_tb.v
@@ -0,0 +1,52 @@
+module song_reader_tb();
+
+    reg clk, reset, play, note_done;
+    reg [1:0] song;
+    wire [5:0] note;
+    wire [5:0] duration;
+    wire song_done, new_note;
+
+    song_reader dut(
+        .clk(clk),
+        .reset(reset),
+        .play(play),
+        .song(song),
+        .song_done(song_done),
+        .note(note),
+        .duration(duration),
+        .new_note(new_note),
+        .note_done(note_done)
+    );
+
+    // Clock and reset
+    initial begin
+        clk = 1'b0;
+        reset = 1'b1;
+        repeat (4) #5 clk = ~clk;
+        reset = 1'b0;
+        forever #5 clk = ~clk;
+    end
+
+    // Tests
+    initial begin
+        
+        song = 2'b00;
+        play = 1;
+        note_done = 0;
+        #20
+        note_done = 1;
+        forever #15 note_done = ~note_done;
+        
+        #100 // test wrap-around
+        song = 2'b11;
+        #1000;
+        
+        
+        
+        
+
+    end
+
+endmodule
+
+
